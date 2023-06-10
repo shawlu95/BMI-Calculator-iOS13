7d0b4d71672fe9e3dcf50589b756b679a36ea8d1
Shaw Lu <shawlu95@gmail.com>
2023-06-09 22:44:13 -0700

    link up UI components

--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
